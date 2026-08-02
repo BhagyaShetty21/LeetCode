8f5562a97a0bde5dc73603f7b6aa91542d0fb543
Bhagya_Shetty <shetty21bhagya@gmail.com>
2026-08-02 22:58:33 +0530

    Time: 2 ms (67.74%) | Memory: 46.6 MB (45.27%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,34 +1,31 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<Character>set = new HashSet<Character>();
+        HashSet<Character>rowSet = new HashSet<Character>();
+        HashSet<Character>columnSet = new HashSet<Character>();
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
                 if(board[i][j]!='.'){
-                    if(!set.add(board[i][j])) return false;
+                    if(!rowSet.add(board[i][j])) return false;
                 }
-            }
-            set.clear();
-        }
-
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
                 if(board[j][i]!='.'){
-                    if(!set.add(board[j][i])) return false;
+                    if(!columnSet.add(board[j][i])) return false;
                 }
             }
-            set.clear();
+            rowSet.clear();
+            columnSet.clear();
         }
 
         for(int i=0;i<9;i++){
             for(int j=3*(i/3);j<(3*(i/3))+3;j++){
                 for(int k=(3*i)%9;k<((3*i)%9)+3;k++){
                     if(board[j][k]!='.'){
-                        if(!set.add(board[j][k])) return false;
+                        if(!rowSet.add(board[j][k])) return false;
                     }
                 }
             }
-            set.clear();
+            rowSet.clear();
         }
+
         return true;
     }
 }
